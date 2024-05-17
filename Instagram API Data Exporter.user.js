46470946d62fa8e8da123f0d067755fc93aa3a50
23233 <23233@users.noreply.github.com>
2024-05-17 11:21:41 +0000

    deploy: 70f4dcdb0242b267a972fdb6e8e567a082c2c682

--- Instagram API Data Exporter.user.js
+++ Instagram API Data Exporter.user.js
@@ -21,12 +21,14 @@
     const originalOpen = unsafeWindow.XMLHttpRequest.prototype.open;
     unsafeWindow.XMLHttpRequest.prototype.open = function(method, url) {
         this.addEventListener('load', function() {
-            if (url.includes('/api/graphql')) {
+            if (url.includes('/api/graphql') || url.includes('/graphql/query')) {
                 try {
                     const response = JSON.parse(this.responseText);
-                    const edges = response?.data?.xdt_api__v1__feed__user_timeline_graphql_connection?.edges;
-                    if (edges && Array.isArray(edges)) {
-                        edges.forEach(edge => {
+
+                    // Check for the first type of data structure
+                    const edges1 = response?.data?.xdt_api__v1__feed__user_timeline_graphql_connection?.edges;
+                    if (edges1 && Array.isArray(edges1)) {
+                        edges1.forEach(edge => {
                             const node = edge.node;
                             const id = node.id;
                             const ownerId = node.owner.id;
@@ -39,8 +41,27 @@
                                 });
                             }
                         });
-                        updateButton();
                     }
+
+                    // Check for the second type of data structure
+                    const edges2 = response?.data?.xdt_api__v1__clips__user__connection_v2?.edges;
+                    if (edges2 && Array.isArray(edges2)) {
+                        edges2.forEach(edge => {
+                            const node = edge.node.media;
+                            const id = node.id;
+                            const ownerId = node.user.id;
+                            const videoUrl = node.video_versions?.[0]?.url;
+                            if (id && ownerId && videoUrl) {
+                                dataToExport.push({
+                                    id: id,
+                                    ownerId: ownerId,
+                                    videoUrl: videoUrl
+                                });
+                            }
+                        });
+                    }
+
+                    updateButton();
                 } catch (e) {
                     console.error('Error parsing response:', e);
                 }
@@ -63,7 +84,10 @@
     button.style.cursor = 'pointer';
     button.innerText = 'Export Feed';
 
-    document.body.appendChild(button);
+    window.addEventListener('load', ()=>{
+        document.body.appendChild(button);
+    })
+
 
     button.addEventListener('click', () => {
         if (dataToExport.length > 0) {
